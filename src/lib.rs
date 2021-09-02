2ec8b141cc1c31caf2b0959b4ef40ea80211fb46
Marcus Borkenhagen <m@fritschy.de>
2021-09-02 09:02:23 +0200

    Implement from a grammar discovered with pest.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,18 @@
-use tracing::{error as log_error, Level};
+use std::collections::HashMap;
+use std::fmt::Debug;
+use std::marker::PhantomData;
 
-// A filtering grammar... (Note: I am not even sure this CAN work)
-//
-//
-// FILTER = EXPR
-// EXPR = '(' EXPR ')'
-//      | UNOP EXPR
-//      | PRIM
-//      | SECONDARY
-//      | COMPARISON
-//      | LOGIC
-//      | IDENTIFIER '(' EXPR ')'
-//      | CONST
-//      | IDENTIFIER
-// UNOP = '!' | '-' | '~'
-// PRIM = EXPR '*' EXPR
-//      | EXPR '/' EXPR
-// SECONDARY = EXPR '+' EXPR
-//           | EXPR '-' EXPR
-// COMPARISON = EXPR COMPOP EXPR
-// COMPOP = '>' | '<' | '==' | '>=' | '<=' | '!='
-// LOGIC = EXPR '&&' EXPR
-//       | EXPR '||' EXPR
-// CONST = NUMBER | STRING
-// STRING = any c string literal?
-// NUMBER = any c numeric literal?
-//
-//
+use nom::branch::alt;
+use nom::bytes::complete::take_till1;
+use nom::character::complete::{multispace0, satisfy};
+use nom::character::{is_alphanumeric, is_hex_digit};
+use nom::combinator::{fail, peek, recognize};
+use nom::error::ParseError;
+use nom::number::complete::recognize_float;
+use nom::{bytes::complete::tag, AsChar, IResult};
+use tracing::{error as log_error, Level};
+use tracing::{info, trace};
+
 // Examples:
 // ctx =~ "AP.*" || ctx == "MAP"             # match ctx against re, or ctx equals to "MAP"
 // app == "REND" && payload =~ ".*error.*"   # app == "REND" AND payload contains "error"
@@ -41,245 +27,280 @@ use tracing::{error as log_error, Level};
 //
 // How to implement chaining of logic operators, i.e. EXPR && EXPR?
 
-use nom::{IResult, bytes::complete::{
-    tag
-}, AsChar};
+pub mod ast {
+    use nom::combinator::{map, eof};
+    use nom::sequence::delimited;
 
-use std::collections::HashMap;
-use std::fmt::Debug;
-use nom::character::complete::{multispace0, satisfy};
-use nom::combinator::{peek, fail, recognize};
-use nom::bytes::complete::take_till1;
-use nom::character::{is_alphanumeric, is_hex_digit};
-use std::marker::PhantomData;
-use nom::branch::alt;
+    use crate::ast::Node::{Binary, Constant, Identifier, Unary};
 
-use tracing::{
-    trace,
-    info,
-};
-use nom::number::complete::recognize_float;
-use nom::error::ParseError;
+    use super::*;
 
-pub trait Filter<T: Debug> : Debug {
-    fn eval(&self, item: &T) -> bool;
-}
+    #[derive(Debug, Clone)]
+    pub enum Node {
+        Unary {
+            op: UnaryOp,
+            expr: Box<Node>,
+        },
+        Binary {
+            lhs: Box<Node>,
+            op: BinaryOp,
+            rhs: Box<Node>,
+        },
+        Identifier(String),
+        Constant(String),
+    }
 
-#[derive(Debug)]
-pub struct Identifier {
-    name: String,
-}
+    #[derive(Debug, Clone)]
+    pub enum UnaryOp {
+        Not, // !A
+        Neg, // -A
+    }
 
-impl Identifier {
-    fn new(name: &str) -> Self {
-        Self {
-            name: name.to_string()
+    // Do we need this.... now?
+    #[derive(Debug, Clone)]
+    pub enum BinaryOp {
+        Eq, // A == B
+        Ne, // A != B
+        Gt, // A >  B
+        Lt, // A <  B
+        Ge, // A >= B
+        Le, // A <= B
+
+        Match, // A =~ B
+
+        Band, // A &  B
+        Bor, // A |  B
+
+        And, // A && B
+        Or, // A || B
+    }
+
+    // A pest.rs grammar
+    //
+    // expr = { SOI ~ simpleExpr ~ EOI }
+    // simpleExpr = { andExpr ~ or ~ simpleExpr | andExpr }
+    // andExpr = { relExpr ~ and ~ andExpr | relExpr }
+    // relExpr = { sumExpr ~ relop ~ relExpr | sumExpr }
+    // sumExpr = { unaryExpr ~ sumop ~ sumExpr | unaryExpr }
+    // relop = { "==" | "!=" | ">=" | "<=" | ">" | "<" | "=~" }
+    // sumop = { "&" }
+    // unaryExpr = { unaryOp ~ unaryExpr | factor }
+    // unaryOp = { "!" | "-" }
+    // factor = { identifier | numeral | parensExpr }
+    // parensExpr = { "(" ~ expr ~ ")" }
+    // identifier = @{ name }
+    // nameHead = { ASCII_ALPHA | "_" }
+    // nameTail = { ASCII_ALPHANUMERIC | "_" }
+    // name = { nameHead ~ nameTail* | nameHead+ }
+    // numeral = @{ ("0x" ~ ASCII_HEX_DIGIT+) | ASCII_DIGIT+ }
+    // or = { "||" }
+    // and = { "&&" }
+    // WHITESPACE = _{ " " | "\t" }
+    //
+    // Example: a_1 & 0x10a != 0 || (ts > 1000 && ts < 99999)
+    // - simpleExpr
+    //   - andExpr > relExpr
+    //     - sumExpr
+    //       - unaryExpr > factor > identifier: "a_1"
+    //       - sumop: "&"
+    //       - sumExpr > unaryExpr > factor > numeral: "0x10a"
+    //     - relop: "!="
+    //     - relExpr > sumExpr > unaryExpr > factor > numeral: "0"
+    //   - or: "||"
+    //   - simpleExpr > andExpr > relExpr > sumExpr > unaryExpr > factor > parensExpr > expr > simpleExpr > andExpr
+    //     - relExpr
+    //       - sumExpr > unaryExpr > factor > identifier: "ts"
+    //       - relop: ">"
+    //       - relExpr > sumExpr > unaryExpr > factor > numeral: "1000"
+    //     - and: "&&"
+    //     - andExpr > relExpr
+    //       - sumExpr > unaryExpr > factor > identifier: "ts"
+    //       - relop: "<"
+    //       - relExpr > sumExpr > unaryExpr > factor > numeral: "99999"
+
+    impl From<&str> for BinaryOp {
+        fn from(i: &str) -> Self {
+            match i {
+                "==" => BinaryOp::Eq,
+                "!=" => BinaryOp::Ne,
+                ">=" => BinaryOp::Ge,
+                "<=" => BinaryOp::Le,
+                ">" => BinaryOp::Gt,
+                "<" => BinaryOp::Lt,
+                "=~" => BinaryOp::Match,
+                "&" => BinaryOp::Band,
+                "|" => BinaryOp::Bor,
+                "&&" => BinaryOp::And,
+                "||" => BinaryOp::Or,
+                _ => panic!("Unknown operator {}", i),
+            }
         }
     }
-}
 
-impl<T: Debug> Filter<T> for Identifier {
-    fn eval(&self, item: &T) -> bool {
-        return true;
-    }
-}
-
-#[derive(Debug)]
-pub struct Constant {
-    value: String,
-}
-
-impl Constant {
-    fn new(name: &str) -> Self {
-        Self {
-            value: name.to_string()
+    impl From<&str> for UnaryOp {
+        fn from(i: &str) -> Self {
+            match i {
+                "!" => UnaryOp::Not,
+                "-" => UnaryOp::Neg,
+                _ => panic!("Unaknown operator {}", i),
+            }
         }
     }
-}
 
-impl<T: Debug> Filter<T> for Constant {
-    fn eval(&self, item: &T) -> bool {
-        return true;
-    }
-}
+    impl Node {
+        fn from_identifier(i: &str) -> Box<Node> {
+            Box::new(Node::Identifier(i.to_string()))
+        }
 
-#[derive(Debug)]
-struct UnaryOp<T: Debug> {
-    op: String,
-    expr: Box<dyn Filter<T>>,
-}
-
-impl<T: Debug> UnaryOp<T> {
-    fn new(op: &str, expr: Box<dyn Filter<T>>) -> Self {
-        Self {
-            op: op.to_string(),
-            expr,
+        fn from_numeric(i: &str) -> Box<Node> {
+            Box::new(Node::Constant(i.to_string()))
         }
     }
-}
 
-impl<T: Debug> Filter<T> for UnaryOp<T> {
-    fn eval(&self, item: &T) -> bool {
-        todo!()
+    // What we want:
+    // A & 0x100 != 0    ->    Binary{Binary{'A', Band, 0x100}, Ne, 0}
+
+    pub fn parse(i: &str) -> IResult<&str, Box<Node>> {
+        trace!("parse: i={}", i);
+        let (i, e) = expr(i)?;
+        let (i, _) = eof(i)?;
+        Ok((i, e))
     }
-}
 
-#[derive(Debug)]
-struct BinaryOp<T: Debug> {
-    left: Box<dyn Filter<T>>,
-    op: String,
-    right: Box<dyn Filter<T>>,
-}
-
-impl<T: Debug> BinaryOp<T> {
-    fn new(left: Box<dyn Filter<T>>, op: &str, right: Box<dyn Filter<T>>) -> Self {
-        Self {
-            left,
-            op: op.to_string(),
-            right,
-        }
+    fn expr(i: &str) -> IResult<&str, Box<Node>> {
+        trace!("expr: i={}", i);
+        simple_expr(i)
     }
-}
 
-impl<T: Debug> Filter<T> for BinaryOp<T> {
-    fn eval(&self, item: &T) -> bool {
-        todo!()
-    }
-}
-
-pub struct FilterContext<T> {
-    constants: HashMap<String, String>,
-    identifiers: HashMap<String, Box<dyn Fn(&T) -> String>>,
-    functions: HashMap<String, Box<dyn Filter<T>>>,  // do we really want/can do this?!
-}
-
-pub fn parse_filter<T: 'static + Debug>(i: &str) -> IResult<&str, Box<dyn Filter<T>>> {
-    trace!("parse_filter: i={}", i);
-    let (i, expr) = parse_chained_expr(i)?;
-    Ok((i, expr))
-}
-
-fn parse_logic_part<T: 'static + Debug>(i: &str) -> IResult<&str, (&str, Box<dyn Filter<T>>)> {
-    trace!("parse_logic_part: i={}", i);
-    let (i, _) = multispace0(i)?;
-    let (i, op) = alt((tag("&&"), tag("||")))(i)?;
-    let (i, right) = parse_chained_expr(i)?;
-    Ok((i, (op, right)))
-}
-
-fn parse_chained_expr<T: 'static + Debug>(i: &str) ->  IResult<&str, Box<dyn Filter<T>>> {
-    trace!("parse_chained_expr: i={}", i);
-    let (i, expr) = parse_expr(i)?;
-    if let Ok((i, (op, right))) = parse_logic_part(i) {
-        Ok((i, Box::new(BinaryOp::<T>::new(expr, op, right)) as Box<dyn Filter<T>>))
-    } else {
-        Ok((i, expr))
-    }
-}
-
-fn parse_expr<T: 'static + Debug>(i: &str) -> IResult<&str, Box<dyn Filter<T>>> {
-    trace!("parse_expr: i={}", i);
-    let (i, _) = multispace0(i)?;
-    // Is this a parenthesized expression?
-    if let Ok((i, _)) = tag::<&str, &str, nom::error::Error<&str>>("(")(i) {
-        let (i, expr) = parse_chained_expr(i)?;
+    fn simple_expr(i: &str) -> IResult<&str, Box<Node>> {
+        trace!("simple_expr: i={}", i);
         let (i, _) = multispace0(i)?;
-        let (i, _) = tag(")")(i)?;
-        return Ok((i, expr));
+        alt((
+            move |i| {
+                let (i, ae) = and_expr(i)?;
+                let (i, _) = multispace0(i)?;
+                let (i, op) = map(tag("||"), BinaryOp::from)(i)?;
+                let (i, se) = simple_expr(i)?;
+                Ok((i, Box::new(Binary { lhs: ae, op, rhs: se, }), ))
+            },
+            and_expr,
+        ))(i)
     }
 
-    if let Ok((i, unop)) = parse_unary_op::<T>(i) {
-        return Ok((i, unop));
+    fn and_expr(i: &str) -> IResult<&str, Box<Node>> {
+        trace!("and_expr: i={}", i);
+        let (i, _) = multispace0(i)?;
+        alt((
+            move |i| {
+                let (i, re) = rel_expr(i)?;
+                let (i, _) = multispace0(i)?;
+                let (i, op) = map(tag("&&"), BinaryOp::from)(i)?;
+                let (i, ae) = and_expr(i)?;
+                Ok((i, Box::new(Binary { lhs: re, op, rhs: ae, }), ))
+            },
+            rel_expr,
+        ))(i)
     }
 
-    if let Ok((i, ident)) = parse_ident::<T>(i) {
-        if let Ok((i, (op, prim))) = parse_primary_part::<T>(i) {
-            return Ok((i, Box::new(BinaryOp::<T>::new(ident, op, prim))));
-        }
-
-        if let Ok((i, (op, prim))) = parse_secondary_part::<T>(i) {
-            return Ok((i, Box::new(BinaryOp::<T>::new(ident, op, prim))));
-        }
-
-        if let Ok((i, (op, prim))) = parse_comparison_part::<T>(i) {
-            return Ok((i, Box::new(BinaryOp::<T>::new(ident, op, prim))));
-        }
-
-        return Ok((i, ident));
+    fn rel_expr(i: &str) -> IResult<&str, Box<Node>> {
+        trace!("rel_expr: i={}", i);
+        let (i, _) = multispace0(i)?;
+        alt((
+            move |i| {
+                let (i, se) = sum_expr(i)?;
+                let (i, _) = multispace0(i)?;
+                let (i, op) = map(relop, BinaryOp::from)(i)?;
+                let (i, re) = rel_expr(i)?;
+                Ok((i, Box::new(Binary { lhs: se, op, rhs: re, }), ))
+            },
+            sum_expr,
+        ))(i)
     }
 
-    // There is nothing we can do here
-    fail(i)
-}
-
-fn parse_primary_part<T: 'static + Debug>(i: &str) -> IResult<&str, (&str, Box<dyn Filter<T>>)> {
-    trace!("parse_primary_part: i={}", i);
-    let (i, _) = multispace0(i)?;
-    let (i, op) = alt((tag("*"), tag("/")))(i)?;
-    let (i, right) = parse_expr(i)?;
-    Ok((i, (op, right)))
-}
-
-fn parse_comparison_part<T: 'static + Debug>(i: &str) -> IResult<&str, (&str, Box<dyn Filter<T>>)> {
-    trace!("parse_comparison_part: i={}", i);
-    let (i, _) = multispace0(i)?;
-    let (i, op) = alt((tag("=="), tag("!="), tag(">="), tag("<="), tag(">"), tag("<")))(i)?;
-    let (i, right) = parse_expr(i)?;
-    Ok((i, (op, right)))
-}
-
-fn parse_secondary_part<T: 'static + Debug>(i: &str) -> IResult<&str, (&str, Box<dyn Filter<T>>)> {
-    trace!("parse_secondary_part: i={}", i);
-    let (i, _) = multispace0(i)?;
-    let (i, op) = alt((tag("+"), tag("-"), tag("&"), tag("|"), tag("^")))(i)?;
-    let (i, right) = parse_expr(i)?;
-    Ok((i, (op, right)))
-}
-
-fn parse_unary_op<T: 'static + Debug>(i: &str) -> IResult<&str, Box<dyn Filter<T>>> {
-    trace!("parse_unary_op: i={}", i);
-    let (i, _) = multispace0(i)?;
-    let (i, op) = alt((tag("!"), tag("-"), tag("~")))(i)?;
-    let (i, _) = multispace0(i)?;
-    let (i, expr) = parse_expr(i)?;
-    Ok((i, Box::new(UnaryOp::<T>::new(op, expr))))
-}
-
-fn identifier(i: &str) -> IResult<&str, &str> {
-    trace!("identifier: i={}", i);
-    let (i, _) = multispace0(i)?;
-    let (i, _) = peek(satisfy(|c| c.is_alpha() || c == '_'))(i)?;
-    let (i, ident) = take_till1(|c| !(is_alphanumeric(c as u8) || c == '_'))(i)?;
-    let (i, _) = multispace0(i)?;
-    Ok((i, ident))
-}
-
-fn parse_ident<T: Debug>(i: &str) -> IResult<&str, Box<dyn Filter<T>>> {
-    trace!("parse_ident: i={}", i);
-    if let Ok((i, num)) = numeric(i) {
-        Ok((i, Box::new(Constant::new(num))))
-    } else {
-        let (i, ident) = identifier(i)?;
-        Ok((i, Box::new(Identifier::new(ident))))
+    fn sum_expr(i: &str) -> IResult<&str, Box<Node>> {
+        trace!("sum_expr: i={}", i);
+        let (i, _) = multispace0(i)?;
+        alt((
+            move |i| {
+                let (i, ue) = unary_expr(i)?;
+                let (i, _) = multispace0(i)?;
+                let (i, op) = map(tag("&"), BinaryOp::from)(i)?;
+                let (i, se) = sum_expr(i)?;
+                Ok((i, Box::new(Binary { lhs: ue, op, rhs: se, }), ))
+            },
+            unary_expr,
+        ))(i)
     }
-}
 
-fn numeric(i: &str) -> IResult<&str, &str> {
-    trace!("numeric: i={}", i);
-    let (i, _) = multispace0(i)?;
-    let (i, num) = alt((hexnum, recognize_float))(i)?;
-    Ok((i, num))
-}
+    fn unary_expr(i: &str) -> IResult<&str, Box<Node>> {
+        trace!("unary_expr: i={}", i);
+        let (i, _) = multispace0(i)?;
+        alt((
+            move |i| {
+                let (i, op) = alt((tag("!"), tag("-")))(i)?;
+                let (i, ue) = unary_expr(i)?;
+                Ok((i, Box::new(Unary { expr: ue, op: UnaryOp::from(op), }), ))
+            },
+            factor,
+        ))(i)
+    }
 
-fn hexnum(i: &str) -> IResult<&str, &str> {
-    trace!("hexnum: i={}", i);
-    let (i, _) = multispace0(i)?;
-    let (i, num) = recognize(|i| {
-        trace!("recognize_hex: i={}", i);
-        let (i, _) = tag("0x")(i)?;
-        take_till1(|x| !is_hex_digit(x as u8))(i)
-    })(i)?;
-    Ok((i, num))
+    fn factor(i: &str) -> IResult<&str, Box<Node>> {
+        trace!("factor: i={}", i);
+        let (i, _) = multispace0(i)?;
+        let (i, f) = alt((identifier, numeric, parens_expr))(i)?;
+        Ok((i, f))
+    }
+
+    fn parens_expr(i: &str) -> IResult<&str, Box<Node>> {
+        trace!("parens_expr: i={}", i);
+        let (i, _) = multispace0(i)?;
+        delimited(tag("("), simple_expr, tag(")"))(i)
+    }
+
+    fn relop(i: &str) -> IResult<&str, BinaryOp> {
+        trace!("relop: i={}", i);
+        let (i, _) = multispace0(i)?;
+        map(
+            alt((
+                tag("=="),
+                tag("!="),
+                tag(">="),
+                tag(">"),
+                tag("<="),
+                tag("<"),
+                tag("=~"),
+            )),
+            BinaryOp::from,
+        )(i)
+    }
+
+    fn identifier(i: &str) -> IResult<&str, Box<Node>> {
+        trace!("identifier: i={}", i);
+        let (i, _) = multispace0(i)?;
+        let (i, _) = peek(satisfy(|c| c.is_alpha() || c == '_'))(i)?;
+        let (i, ident) = take_till1(|c| !(is_alphanumeric(c as u8) || c == '_'))(i)?;
+        let (i, _) = multispace0(i)?;
+        Ok((i, Box::new(Identifier(ident.to_string()))))
+    }
+
+    fn numeric(i: &str) -> IResult<&str, Box<Node>> {
+        trace!("numeric: i={}", i);
+        let (i, _) = multispace0(i)?;
+        let (i, num) = alt((hexnum, recognize_float))(i)?;
+        Ok((i, Box::new(Constant(num.to_string()))))
+    }
+
+    fn hexnum(i: &str) -> IResult<&str, &str> {
+        trace!("hexnum: i={}", i);
+        let (i, _) = multispace0(i)?;
+        let (i, num) = recognize(|i| {
+            trace!("recognize_hex: i={}", i);
+            let (i, _) = tag("0x")(i)?;
+            take_till1(|x| !is_hex_digit(x as u8))(i)
+        })(i)?;
+        Ok((i, num))
+    }
 }
 
 #[cfg(test)]
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
 use tracing_subscriber::FmtSubscriber;
 use tracing::{Level, info, error as log_err};
-use filterer::parse_filter;
+use filterer::{ast};
 use std::io;
+use std::io::BufRead;
 
 fn main() -> io::Result<()> {
     // a builder for `FmtSubscriber`.
     let subscriber = FmtSubscriber::builder()
         // all spans/events with a level higher than TRACE (e.g, debug, info, warn, etc.)
         // will be written to stdout.
-        .with_max_level(Level::TRACE)
+        .with_max_level(Level::INFO)
         // completes the builder.
-        .with_thread_names(true)
+        //.with_thread_names(true)
         .finish();
 
     tracing::subscriber::set_global_default(subscriber)
@@ -22,15 +23,29 @@ fn main() -> io::Result<()> {
         app: String,
     }
 
-    parse_filter::<Message>("((!(ctx == app)) && (ts > 1000 && ts <= 9999)) || flags & 0x100 != 0").and_then(|x| {
-        info!("Got: {}: {:#?}", x.0, x.1.as_ref());
-        Ok(())
-    }).expect("parsed");
+    if std::env::args().nth(1).unwrap_or("".to_string()) == "-i" {
+        let stdin = std::io::stdin();
+        let stdin = stdin.lock();
+        for l in stdin.lines() {
+            if l.is_err() {
+                break;
+            }
 
-    parse_filter::<Message>("flags & 0x100 != 0").and_then(|x| {
-        info!("Got: {}: {:#?}", x.0, x.1.as_ref());
-        Ok(())
-    }).expect("parsed");
+            let l = l.unwrap();
+
+            if let Err(e) = ast::parse(l.trim()).and_then(|x| {
+                info!("Got: {}: {:#?}", x.0, x.1.as_ref());
+                Ok(())
+            }) {
+                log_err!("Got error: {:?}", e);
+            }
+        }
+    } else {
+        ast::parse("(flags & 0x100) != 0 && ts <= 10101").and_then(|x| {
+            info!("Got: {}: {:#?}", x.0, x.1.as_ref());
+            Ok(())
+        }).expect("parsed");
+    }
 
     Ok(())
 }
