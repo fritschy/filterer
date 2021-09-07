7617fa561ae7d776f666b4ca1353bcb3e0448056
Marcus Borkenhagen <m@fritschy.de>
2021-09-07 08:39:59 +0200

    eval: move Value impl out of Eval::filter_eval()

--- src/eval.rs
+++ src/eval.rs
@@ -16,123 +16,123 @@ pub trait Eval<T> {
     fn eval_filter(&self, e: T) -> bool;
 }
 
+enum Value<'a> {
+    Int(isize),
+    Str(&'a str),
+    Re(&'a Regex),
+}
+
+impl<'a> From<bool> for Value<'a> {
+    fn from(b: bool) -> Self {
+        if b { Self::Int(1) }
+        else { Self::Int(0) }
+    }
+}
+
+impl<'a> PartialEq for Value<'a> {
+    fn eq(&self, other: &Self) -> bool {
+        if self.is_re() || other.is_re() {
+            return false;
+        }
+
+        let b_i = other.is_int();
+        let b_s = other.is_str();
+
+        // allow comparison of nums and strings
+        if let Value::Int(a) = self {
+            if b_i {
+                return *a == other.as_int();
+            } else if b_s {
+                return *a == parse_num(other.as_str());
+            }
+        } else if let Value::Str(a) = self {
+            if b_s {
+                return *a == other.as_str();
+            } else if b_i {
+                return parse_num(a) == other.as_int();
+            }
+        }
+
+        false
+    }
+}
+
+impl<'a> Value<'a> {
+    fn as_int(&self) -> isize {
+        match self {
+            Value::Int(x) => *x,
+            Value::Str(x) => {
+                tracing::warn!("as_int() from string!");
+                parse_num(*x)
+            },
+            _ => panic!("as_int() needs to be a number"),
+        }
+    }
+
+    fn as_bool(&self) -> bool {
+        match self {
+            Value::Int(x) => *x != 0,
+            Value::Str(s) => *s != "0",
+            _ => false,
+        }
+    }
+
+    fn as_str(&self) -> &str {
+        match self {
+            Value::Str(s) => *s,
+            Value::Int(0) | Value::Re(_) => {
+                tracing::warn!("as_str on int or re");
+                "0"
+            }
+            Value::Int(_) => {
+                tracing::warn!("as_str on int");
+                "1"
+            }
+        }
+    }
+
+    fn as_re(&self) -> &Regex {
+        match self {
+            Value::Re(r) => *r,
+            _ => panic!("Not an re"),
+        }
+    }
+
+    fn is_int(&self) -> bool {
+        matches!(self, Value::Int(_))
+    }
+
+    fn is_str(&self) -> bool {
+        matches!(self, Value::Str(_))
+    }
+
+    fn is_re(&self) -> bool {
+        matches!(self, Value::Re(_))
+    }
+}
+
+fn value<'a>(node: &'a Node, e: &'a dyn Accessor) -> Option<Value<'a>> {
+    match node {
+        Node::StringLiteral(s) => Some(Value::Str(s)),
+        Node::Constant(s) => Some(Value::Int(*s)),
+        Node::Identifier(s) => {
+            if let Ok(num) = e.get_num(s) {
+                Some(Value::Int(num))
+            } else if let Ok(s) = e.get_str(s) {
+                Some(Value::Str(s))
+            } else {
+                None
+            }
+        },
+        Node::Regexp(re) => Some(Value::Re(re)),
+        _ => unreachable!(),
+    }
+}
+
+
 // FIXME: this is quickest and most inefficient way I could possibly imagine!
 impl Eval<&dyn Accessor> for Box<Node> {
     fn eval_filter(&self, e: &dyn Accessor) -> bool {
-        #[derive(Debug)]
-        enum Value<'a> {
-            Int(isize),
-            Str(&'a str),
-            Re(&'a Regex),
-        }
-
-        impl<'a> From<bool> for Value<'a> {
-            fn from(b: bool) -> Self {
-                if b { Self::Int(1) }
-                else { Self::Int(0) }
-            }
-        }
-
-        impl<'a> PartialEq for Value<'a> {
-            fn eq(&self, other: &Self) -> bool {
-                if self.is_re() || other.is_re() {
-                    return false;
-                }
-
-                let b_i = other.is_int();
-                let b_s = other.is_str();
-
-                // allow comparison of nums and strings
-                if let Value::Int(a) = self {
-                    if b_i {
-                        return *a == other.as_int();
-                    } else if b_s {
-                        return *a == parse_num(other.as_str());
-                    }
-                } else if let Value::Str(a) = self {
-                    if b_s {
-                        return *a == other.as_str();
-                    } else if b_i {
-                        return parse_num(a) == other.as_int();
-                    }
-                }
-
-                false
-            }
-        }
-
-        impl<'a> Value<'a> {
-            fn as_int(&self) -> isize {
-                match self {
-                    Value::Int(x) => *x,
-                    Value::Str(x) => {
-                        tracing::warn!("as_int() from string!");
-                        parse_num(*x)
-                    },
-                    _ => panic!("as_int() needs to be a number"),
-                }
-            }
-
-            fn as_bool(&self) -> bool {
-                match self {
-                    Value::Int(x) => *x != 0,
-                    Value::Str(s) => *s != "0",
-                    _ => false,
-                }
-            }
-
-            fn as_str(&self) -> &str {
-                match self {
-                    Value::Str(s) => *s,
-                    Value::Int(0) | Value::Re(_) => {
-                        tracing::warn!("as_str on int or re");
-                        "0"
-                    }
-                    Value::Int(_) => {
-                        tracing::warn!("as_str on int");
-                        "1"
-                    }
-                }
-            }
-
-            fn as_re(&self) -> &Regex {
-                match self {
-                    Value::Re(r) => *r,
-                    _ => panic!("Not an re"),
-                }
-            }
-
-            fn is_int(&self) -> bool {
-                matches!(self, Value::Int(_))
-            }
-
-            fn is_str(&self) -> bool {
-                matches!(self, Value::Str(_))
-            }
-
-            fn is_re(&self) -> bool {
-                matches!(self, Value::Re(_))
-            }
-        }
-
-        fn value<'a>(node: &'a Node, e: &'a dyn Accessor) -> Option<Value<'a>> {
-            match node {
-                Node::StringLiteral(s) => Some(Value::Str(s)),
-                Node::Constant(s) => Some(Value::Int(*s)),
-                Node::Identifier(s) => {
-                    if let Ok(num) = e.get_num(s) {
-                        Some(Value::Int(num))
-                    } else if let Ok(s) = e.get_str(s) {
-                        Some(Value::Str(s))
-                    } else {
-                        None
-                    }
-                },
-                Node::Regexp(re) => Some(Value::Re(re)),
-                _ => unreachable!(),
-            }
-        }
-
         fn eval<'a>(node: &'a Node, e: &'a dyn Accessor) -> Value<'a> {
             tracing::trace!("eval, node={:?}", node);
 
