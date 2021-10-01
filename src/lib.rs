d348a7f590d09cbc416e0f8da44fb70e6aa5afdf
Marcus Borkenhagen <m@fritschy.de>
2021-10-01 07:32:42 +0200

    lib: remove obsolete and useless header comment

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,3 @@
-// Examples:
-// ctx =~ "AP.*" || ctx == "MAP"             # match ctx against re, or ctx equals to "MAP"
-// app == "REND" && payload =~ ".*error.*"   # app == "REND" AND payload contains "error"
-//
-//
-// IDENTIFIERS can be defined freely by first "declaring" them when configuring
-// the filter, i.e. filter.add_define("app", |msg| msg.app) # where msg is something
-// that makes sense in the context.
-//
-// A parsed filter should result in an AST that can be evaluated, don't particularly
-// concern yourself with efficiency, this is a first test.
-//
-// How to implement chaining of logic operators, i.e. EXPR && EXPR?
-
 mod sema;
 mod value;
 
